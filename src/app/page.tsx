8eb3b0387d1148793921b7c8f3fc387ddb4f1700
M.hammad3373 <alihammadg2003@gmail.com>
2024-08-26 20:30:29 +0500

    next-js-2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
       <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-20 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/Snapchat-248175373.jpg" alt="" width="384" height="512"/>
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/Snapchat-248175373.jpg" alt="" width="384" height="512"/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
